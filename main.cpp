7bbdf3ac4f6ae977e993d2b67ad88e673452ce6a
lcwdmg <lcwdmg@umsystem.edu>
2022-01-30 23:21:27 -0600

    successfully reading from file

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <iomanip>
+#include <string>
 
 using namespace std;
 
@@ -10,29 +11,29 @@ int main()
 	const int ARRAY_SIZE = 5;
 	int grades[ARRAY_SIZE] = {1,2,3,4,5};
 	int count = 0;
-	ifstream inputfile;
+	fstream inputfile;
 
 	inputfile.open("file.sp2022");
 
 
 	// reading the file into the array
-	while (count < ARRAY_SIZE && inputfile >> grades[count]) 
-		count++;
+	//while (count < ARRAY_SIZE && inputfile >> grades[count]) 
+	//	count++;
 
     if (inputfile.is_open()) {
-        std::string line;
+        string line;
 
-        std:: cout << "File opening successfull!!" << endl;
-        while (inputfile >> line) {
-        std::cout << "Line 1: " << line << std::endl;
+        cout << "File opening successfull!!" << endl;
+        while (getline(inputfile, line)) { //while ( inputfile >> line) {
+            std::cout << "Line 1: " << line << std::endl;
         }
     } else {
-        std::cerr << "Unable to open ile:  file.sp2022" << std::endl;
+        std::cerr << "Unable to open file:  file.sp2022" << std::endl;
         return -1;
     }   
 
 	inputfile.close();
-    cout << "hi " << endl;
+    std::cout << "hi " << std::endl;
     // for (int i = 0; i < ARRAY_SIZE; i++){
     //     cout << grades[i] << endl;
     // }
