03490c7c012e0864cdb5c7ea92077bdf401b806f
lcwdmg <lcwdmg@umsystem.edu>
2022-02-14 17:52:20 -0600

    update fileName validation for gcc 4.8 compliance (regex was not implemented yet)

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 
 using namespace std;
 
+bool fileNameContainsDot(string FileName);
 string validateFileName(string fileName);
 
 int main(int argc, char **argv) {
@@ -42,17 +43,26 @@ int main(int argc, char **argv) {
     return 0;
 }
 
-string validateFileName(string fileName) {
-    smatch matches;
+bool fileNameContainsDot(string fileName) {
+    for (int i = 0; i < fileName.length(); i++) {
+        if (fileName[i] == '.') {
+            return true;
+        }
+    }
+    return false;
+}
 
-    regex e("^([a-zA-Z0-9_]+)(\\.sp2022)?$");
+string validateFileName(string fileName) {
     
-    if(regex_match(fileName, matches, e)) {
-        
-        return matches[1]; // return just the file name, not the extension 
+    string fileExtension = fileName.substr(fileName.length() - 7, fileName.length());
     
+    //the file should have the extension .sp2022 or no extension at all
+    if (!fileNameContainsDot(fileName)){
+        return fileName;
+    } else if (fileExtension == ".sp2022") {
+        return fileName.substr(0, fileName.length()  - 7);
     } else {
-        cerr << "Bad file name:" << fileName <<  endl;
+        cerr << "Bad file name: " << fileName << endl;
         return "";
     }
 }
